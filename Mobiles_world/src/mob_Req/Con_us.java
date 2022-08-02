ac2380d8bd65cfc7f3ca1dec8895cf1fb6a4d2e7
MANOJKUMARMP <mmp1467@gmail.com>
2022-08-02 15:06:47 +0530

    8th commit

--- Mobiles_world/src/mob_Req/Con_us.java
+++ Mobiles_world/src/mob_Req/Con_us.java
@@ -17,17 +17,23 @@ public class Con_us {
         WebDriver driver=new ChromeDriver();
         driver.get("https://mobileworld.azurewebsites.net");
         driver.findElement(By.xpath("(//a[@id='navbarDropdown'])")).click();
+        Thread.sleep(1000);
         driver.findElement(By.linkText("Contact Us")).click();
+        Thread.sleep(1000);
         Set<String> windows = driver.getWindowHandles();
         Iterator<String> it = windows.iterator();
         String parentId = it.next();
         String childId = it.next();
         driver.switchTo().window(childId);
         driver.manage().window().maximize();
-        driver.findElement(By.xpath("//input[@placeholder='Username']")).sendKeys("chethan");
-        driver.findElement(By.xpath("//input[@placeholder='Email']")).sendKeys("cgowdan@gmail.com");
-        driver.findElement(By.xpath("//input[@placeholder='Phone']")).sendKeys("9663824163");
-        driver.findElement(By.name("message")).sendKeys("hello world");
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//input[@placeholder='Username']")).sendKeys("manojmp");
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//input[@placeholder='Email']")).sendKeys("mmp1467@gmail.com");
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//input[@placeholder='Phone']")).sendKeys("8970712707");
+        Thread.sleep(1000);
+        driver.findElement(By.name("message")).sendKeys("Hi Qudos");
         driver.findElement(By.xpath("//input[@type='submit']")).click();
         driver.quit();
 }
